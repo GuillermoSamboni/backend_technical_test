31f91b9fb18c3346e458ffffb3f12fde23abec6b
GuillermoSamboni <guillermosamboni0912@gmail.com>
2024-05-14 16:04:39 -0500

    added route default

--- src/config/Server.js
+++ src/config/Server.js
@@ -1,7 +1,9 @@
 //region imports
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
 const server = express();
+const routes = express.Router();
+
 const morgan = require("morgan");
 const port = process.env.PORT || 3000;
 const createRole = require("../libs/initialSetup");
@@ -12,8 +14,15 @@ createRole();
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
-
 //end region dependences
+
+//region default-routes
+const path = require("path");
+const defaultRoute = routes.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+server.use(defaultRoute);
+//end region default-routes
 
 //region products-routes
 const routeUsers = require("../routes/Products.routes");
